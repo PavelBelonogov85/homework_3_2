d319ce7b1c13857523089b181479b17c5f3a6109
PavelBelonogov85 <belonogovp@mail.ru>
2022-07-19 13:02:09 +0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,26 @@
+import kotlin.math.max
+import kotlin.random.Random
+
+enum class CardType {
+    VKPAY, MASTERCARD, MAESTRO, OTHER, VISA, MIR
+}
+
+fun main() {
+    val amount:Int = 5_000_50 //Random.nextInt(0, 500_005)
+    val previousTransfersSum:Int = 10_000_00
+
+    val commission:Int = getCommission(CardType.MAESTRO, 100_000_00, 200_00)
+
+    println("Сумма списания: ${amount/100.0} руб., комиссия ${commission/100.0} руб., получателю придет ${(amount-commission)/100.0} руб.")
+}
+
+fun getCommission(cardType:CardType=CardType.VKPAY, previousTransfersSum:Int=0, amount:Int):Int {
+    return when (cardType) {
+        CardType.VKPAY -> 0
+
+        CardType.MASTERCARD, CardType.MAESTRO -> if ((amount+previousTransfersSum)>300_00 && (amount+previousTransfersSum)<75_000_00) {0} else {20_00 + amount/1000*6}
+
+        CardType.VISA, CardType.MIR, CardType.OTHER  -> max(35_00, amount * 75 / 10000)
+
+    }
+}
